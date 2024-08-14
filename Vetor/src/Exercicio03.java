a78cf228d1934c323c0fd7fbaa937bc0d754dbc8
Luis Carlos <133696936+Shadowstk@users.noreply.github.com>
2024-08-14 15:34:35 -0300

    Add files via upload

--- Vetor/src/Exercicio03.java
+++ Vetor/src/Exercicio03.java
@@ -1,10 +1,29 @@
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class Exercicio03 {
 	public static void main(String[] args) {
-		
+
 		Scanner in = new Scanner(System.in);
-		int[] x = new int[10];
-		
+		int[] x = new int[6];
+		double[] media = new double[x.length];
+		double nota; 
+		int medi = 0;
+
+		for (int i = 0; i < x.length; i++) {
+			System.out.println("Aluno " + (i + 1));
+
+			for (int j = 0; j < 4; j++) {
+				System.out.println("Nota ");
+				nota = in.nextDouble();
+				media[i] += nota;
+				
+			}
+			media[i] /= 4;
+			if (media[i] >= 6) {
+				medi++;
+			}
+		}
+		System.out.println("Total de alunos com média maior ou igual a 6.0 = " + medi);
 	}
 }
