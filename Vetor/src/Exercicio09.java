aa0ad501b99a0ca22adfb399f75a246b022dd3a5
Luis Carlos <133696936+Shadowstk@users.noreply.github.com>
2024-08-21 22:33:14 -0300

    Add files via upload

--- Vetor/src/Exercicio09.java
+++ Vetor/src/Exercicio09.java
@@ -1,31 +1,30 @@
-
-import java.util.Iterator;
 import java.util.Scanner;
 
 public class Exercicio09 {
 	public static void main(String[] args) {
 
 		Scanner in = new Scanner(System.in);
-		double media = 0, folhaP = 0;
+		double folhaP = 0;
 
 		System.out.println("Digite o total de funcionários ");
 		int rep = in.nextInt();
 
 		String[] nome = new String[rep];
-
 		int[] salario = new int[rep];
 
 		for (int i = 0; i < rep; i++) {
+			in.nextLine();
 			System.out.println("Digite o seu nome ");
-			nome[i] = in.next();
+			nome[i] = in.nextLine();
 			System.out.println("Digite seu salário ");
 			salario[i] = in.nextInt();
 
 			folhaP += salario[i];
-			media = folhaP / rep;
 
 		}
-		System.out.println("Folha de pagamento é " + folhaP + " A média salaria é de " + media);
+		System.out.println("Folha de pagamento é " + String.format("%.2f", folhaP));
+		System.out.println("A média salaria é de " + String.format("%.2f", folhaP/rep));
 
+		
 	}
 }
